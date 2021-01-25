3814629103fb5f8714fbfccce1e4b32bbd874999
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-01-24 20:56:44 -0500

    filter transactions

--- src/graphql/transactions/transaction.query.ts
+++ src/graphql/transactions/transaction.query.ts
@@ -2,8 +2,20 @@ import gql from 'graphql-tag';
 import {TransactionTypeFragment} from './transaction.fragment';
 
 export const GET_MY_TRANSACTIONS = gql`
-    query Transactions($tradeEnv: String, $owner: String!, $page: Float, $limit: Float) {
-        data: transactions(tradeEnv: $tradeEnv, owner: $owner, page: $page, limit: $limit) {
+    query Transactions(
+        $tradeEnv: String
+        $filter: String
+        $owner: String!
+        $page: Float
+        $limit: Float
+    ) {
+        data: transactions(
+            tradeEnv: $tradeEnv
+            filter: $filter
+            owner: $owner
+            page: $page
+            limit: $limit
+        ) {
             ...TransactionTypeFragment
         }
     }
