6ec13b991bd8a0ede23848edf0299369b46b40a1
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-01 02:18:31 -0500

    add mutations patch

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,6 +2,6 @@ export * from './shared';
 export * from './marketdata';
 export * from './payment';
 export * from './trade';
-export * from './transactions';
+export * from './transaction';
 export * from './user';
 // export * from './withdraw';
---
+++ src/graphql/trade/trade.mutation.ts
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+import {ResTypeFragment} from '../shared';
+
+export const CREATE_PAYMENTMETHOD_MUTATION = gql`
+    mutation CreatePaymentMethod($type: String!, $name: String!, $owner: String!, $info: String!) {
+        data: createPaymentMethod(type: $type, name: $name, owner: $owner, info: $info) {
+            ...ResTypeFragment
+        }
+    }
+    ${ResTypeFragment}
+`;
+
+export const CREATE_WITHDRAWPAYMENT_MUTATION = gql`
+    mutation CreateWithdrawPayment($amount: Float!, $owner: String!, $paymentMethodId: String!) {
+        data: createWithdrawPayment(
+            amount: $amount
+            owner: $owner
+            paymentMethodId: $paymentMethodId
+        ) {
+            ...ResTypeFragment
+        }
+    }
+    ${ResTypeFragment}
+`;
