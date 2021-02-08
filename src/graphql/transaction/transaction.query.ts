99f0ef245eefdfa6bb8258b1f6bef383afa6d7d0
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-08 02:27:32 -0500

    Patch get my transactions

--- src/graphql/transaction/transaction.query.ts
+++ src/graphql/transaction/transaction.query.ts
@@ -2,20 +2,8 @@ import gql from 'graphql-tag';
 import {TransactionTypeFragment} from './transaction.fragment';
 
 export const GET_MY_TRANSACTIONS = gql`
-    query Transactions(
-        $tradeEnv: String
-        $filter: String
-        $owner: String!
-        $page: Float
-        $limit: Float
-    ) {
-        data: transactions(
-            tradeEnv: $tradeEnv
-            filter: $filter
-            owner: $owner
-            page: $page
-            limit: $limit
-        ) {
+    query Transactions($filter: String, $owner: String!, $page: Float, $limit: Float) {
+        data: transactions(filter: $filter, owner: $owner, page: $page, limit: $limit) {
             ...TransactionTypeFragment
         }
     }
