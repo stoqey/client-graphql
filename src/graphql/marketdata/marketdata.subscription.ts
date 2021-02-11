11ed65cc6bdcd8a7ee268a3e7f5b5a6daf13f6b0
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-11 00:57:04 -0500

    patch CURRENCY_SUBSCRIPTION

--- src/graphql/marketdata/marketdata.subscription.ts
+++ src/graphql/marketdata/marketdata.subscription.ts
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 import {MarketDataTypeFragment} from './marketdata.fragment';
 
 export const CURRENCY_SUBSCRIPTION = gql`
-    subscription GetCurrency($symbol: String, $time: String) {
-        data: onCurrency(symbol: $symbol, time: $time) {
+    subscription GetCurrency($symbol: String!) {
+        data: onCurrency(symbol: $symbol) {
             ...MarketDataTypeFragment
         }
     }
