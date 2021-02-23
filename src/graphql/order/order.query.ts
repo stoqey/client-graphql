e7191822a58b44ac089d783ba94aaaaad05d995a
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-23 01:40:19 -0500

    order query patch

---
+++ src/graphql/order/order.query.ts
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+import {OrderTypeFragment} from './order.fragment';
+
+export const GET_MY_ORDERS = gql`
+    query myOrders($owner: String!) {
+        data: myOrders(owner: $owner) {
+            ...OrderTypeFragment
+        }
+    }
+    ${OrderTypeFragment}
+`;
+
+export const GET_ALL_ORDERS = gql`
+    query orders() {
+        data: orders() {
+            ...OrderTypeFragment
+        }
+    }
+    ${OrderTypeFragment}
+`;
