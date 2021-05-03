afd12db3a687fcbc5d3b32dc7aa57fb870cdcef6
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-05-03 01:21:47 -0400

    minor remove unnecessesary args

--- src/graphql/stripe/stripe.query.ts
+++ src/graphql/stripe/stripe.query.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const GET_STRIPECONNECTED_ACCOUNTS = gql`
-    query GetStripeConnectedAccounts($owner: String!, $limit: Float, $page: Float) {
-        data: getStripeConnectedAccounts(owner: $owner, limit: $limit, page: $page)
+    query GetStripeConnectedAccounts($owner: String!) {
+        data: getStripeConnectedAccounts(owner: $owner)
     }
 `;
