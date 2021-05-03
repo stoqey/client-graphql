2638a2076eb3d4558367c4a4329225d4057124d1
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-05-02 22:33:07 -0400

    rename withdraw requestas

--- src/graphql/withdraw/index.ts
+++ src/graphql/withdraw/index.ts
@@ -1,3 +1,3 @@
-export * from './user.mutation';
-export * from './user.query';
+export * from './withdrawrequest.mutation';
+export * from './withdrawrequest.query';
 export * from './withdrawrequest.fragment';
