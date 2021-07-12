b6074ed09920baa4a5a639ebd3294f86c1d5e553
cmuhoza <ceddymuhoza@gmail.com>
2021-07-12 11:30:05 -0400

    OnMapRegion

--- src/graphql/adslisting/adslisting.query.ts
+++ src/graphql/adslisting/adslisting.query.ts
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 import {AdsListingTypeFragment} from './adslisting.fragment';
 
 export const ON_MAP_REGION = gql`
-    query OnMapRegion($lat: Float!, $lon: Float!) {
-        data: onMapRegion(lon: $lon, lat: $lat) {
+    query OnMapRegion($lat: Float!, $lon: Float!, zoom: Float) {
+        data: onMapRegion(lon: $lon, lat: $lat, zoom: $zoom) {
             ...AdsListingTypeFragment
         }
     }
